82d873cd586866429d9e56cb3b2f7b6998324e2d
ldeniau <github@ldeniau.fr>
2018-12-09 20:11:24 +0100

    Trying project lombok on Day3 Square class

--- Day3/src/main/java/Square.java
+++ Day3/src/main/java/Square.java
@@ -1,59 +1,15 @@
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Square {
     private int ID;
     private int x;
     private int y;
     private int width;
     private int height;
-
-    Square(){}
-
-    Square(int ID, int x, int y, int width, int height) {
-        this.ID = ID;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
-
-    public int getID() {
-        return ID;
-    }
-
-    public void setID(int ID) {
-        this.ID = ID;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
 
     public int getX2 () {
         return x + width;
