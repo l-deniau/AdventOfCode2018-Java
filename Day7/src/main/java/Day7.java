1de4796c1936b991c839aa37e57509c88229a204
ldeniau <github@ldeniau.fr>
2018-12-13 17:26:50 +0100

    (WIP) Day7 - Part two

--- Day7/src/main/java/Day7.java
+++ Day7/src/main/java/Day7.java
@@ -1,3 +1,8 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -29,6 +34,13 @@ public class Day7 {
                 "Part One - Ordered steps :"
         );
         System.out.println(getOrderedSteps(stepList));
+
+        System.out.println();
+
+        System.out.println(
+                "Part Two - Time spend to achieve all step with 5 workers :"
+        );
+        System.out.println(getTimedSteps(stepList));
     }
 
     private static String getOrderedSteps(Map<String, Set<String>> stepList) {
@@ -49,5 +61,44 @@ public class Day7 {
             }
         }
         return result.toString();
+    }
+
+    private static int getTimedSteps(Map<String, Set<String>> stepList) {
+        int second = 0;
+
+        List<Worker> workerlist = new ArrayList<>(Arrays.asList(
+                new Worker(1, "", true, 0),
+                new Worker(2, "", true, 0),
+                new Worker(3, "", true, 0),
+                new Worker(4, "", true, 0),
+                new Worker(5, "", true, 0)
+        ));
+
+        return second;
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Setter
+    @Getter
+    private static class Worker {
+        private int ID;
+        private String activeStep;
+        private boolean isIdle;
+        private int startSecond;
+
+        public void work(int currentSecond) {
+            // Determine duration of a step helped by unicode number
+            // example : A unicode equal 65 and A take 61 seconds.
+            // So we just need to subtract 4 to the unicode number
+            int activeStepDuration = this.activeStep.codePointAt(0) - 4;
+            if (currentSecond - startSecond >= activeStepDuration) {
+                this.isIdle = true;
+            };
+        }
+        public void newWork(int currentSecond, String step) {
+            this.activeStep = step;
+            this.isIdle = false;
+        }
     }
 }
